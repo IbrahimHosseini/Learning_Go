06367eb884990f1e8f257d0df5b8b362958916a1
Ibrahim Hosseini <sehosseini@me.com>
2024-05-20 09:14:06 +0330

    create DSA package

---
+++ DSA/linkedList.go
@@ -0,0 +1,9 @@
+package DSA
+
+import (
+	"fmt"
+)
+
+func PrintTest() {
+	fmt.Println("DSA package")
+}
--- main.go
+++ main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"fmt"
+	"myproject/DSA"
 	"myproject/closures"
 )
 
 func main() {
+
+	callClosure()
+
+	callLinkedList()
+}
+
+func callLinkedList() {
+	DSA.PrintTest()
+}
+
+// The function `callClosure` creates a new user with the name "Ibrahim Hossinie" using a closure and
+// then prints the user information.
+func callClosure() {
 	getUser := closures.NewUser("Ibrahim", "Hossinie")
 	fmt.Println(getUser())
 }
